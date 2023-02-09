3908ef9e8c4c4ebdd4f834a65b608fe5cb2f7837
yamaokana <66835626+toroochi@users.noreply.github.com>
2023-02-09 15:54:10 +0900

    add: 鼓動,敵

---
+++ Assets/01. pinfu/Scripts/enemyScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyScript : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    NavMeshAgent agent;
+    float moveSpeed;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        moveSpeed = agent.speed;
+    }
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.position) < 30.0f)
+        {
+            agent.destination = target.position;
+            agent.speed = moveSpeed;
+        }
+        else
+        {
+            agent.speed = 0;
+        }
+    }
+}
