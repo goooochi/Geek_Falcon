66e1596c23b082016a81e44358ef0c37a6e4d8bf
yamaokana <66835626+toroochi@users.noreply.github.com>
2023-02-10 23:07:12 +0900

    add: 敵と鍵のランダムな位置生成,Fadeシステム

---
+++ Assets/01. pinfu/Scripts/enemy_position.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_position : MonoBehaviour
+{
+    public GameObject obj;
+    public List<Transform> positions;
+    void Start()
+    {
+            Vector3 position = positions[Random.Range(0, positions.Count)].position;
+            Instantiate(obj, position, Quaternion.identity);
+    }
+}
