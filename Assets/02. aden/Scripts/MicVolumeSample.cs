96294677a5ad02d23872354d887360a8da85f37e
Frank27 <39018610+goooochi@users.noreply.github.com>
2023-03-06 01:51:43 +0900

    マイク削除

--- Assets/02. aden/Scripts/MicVolumeSample.cs
+++ Assets/02. aden/Scripts/MicVolumeSample.cs
@@ -1,66 +1,83 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using System;
 
-public class MicVolumeSample : MonoBehaviour
-{
-    [SerializeField, Range(0f, 10f)] float m_gain = 1f; // 音量に掛ける倍率
-    float m_volumeRate; // 音量(0-1)
-   [SerializeField] float isTriggerVolume;
-    private bool canTriggerChange = false;
-    private float falseTime=1;
-    // Use this for initialization
-    void Start()
-    {
-        AudioSource aud = GetComponent<AudioSource>();
-        if ((aud != null) && (Microphone.devices.Length > 0)) // オーディオソースとマイクがある
-        {
-            string devName = Microphone.devices[0]; // 複数見つかってもとりあえず0番目のマイクを使用
-            int minFreq, maxFreq;
-            Microphone.GetDeviceCaps(devName, out minFreq, out maxFreq); // 最大最小サンプリング数を得る
-            aud.clip = Microphone.Start(devName, true, 2, minFreq); // 音の大きさを取るだけなので最小サンプリングで十分
-            aud.Play(); //マイクをオーディオソースとして実行(Play)開始
-        }
-    }
+//public class MicVolumeSample : MonoBehaviour
+//{
+//    [SerializeField, Range(0f, 10f)] float m_gain = 1f; 
+//    float m_volumeRate; 
+//   [SerializeField] float isTriggerVolume;
+//    private bool canTriggerChange = false;
+//    private float falseTime=1;
+//    // Use this for initialization
 
-    // Update is called once per frame
-    void Update()
-    {
-        //  Debug.Log(m_volumeRate);
-        if (falseTime > 0)
-        {
-            falseTime -= Time.deltaTime;
-        }
-        else if(canTriggerChange == true && falseTime < 0)
-        {
-            enemyScript.isTrigger = false;
-            canTriggerChange = false;
-        }
-    }
+//#if UNITY_WEBGL && !UNITY_EDITOR
+//        void Awake()
+//        {
+//            Microphone.Init();
+//            Microphone.QueryAudioInput();
+//        }
+//#endif
 
-    // オーディオが読まれるたびに実行される
-    private void OnAudioFilterRead(float[] data, int channels)
-    {
-        float sum = 0f;
-        for (int i = 0; i < data.Length; ++i)
-        {
-            sum += Mathf.Abs(data[i]); // データ（波形）の絶対値を足す
-        }
-        // データ数で割ったものに倍率をかけて音量とする
-        m_volumeRate = Mathf.Clamp01(sum * m_gain / (float)data.Length);
-        if (m_volumeRate > isTriggerVolume )
-        {
-            enemyScript.isTrigger = true;
-            canTriggerChange = true;
-            falseTime = 10;
-            Debug.Log("VolT");
-        }
-        else if(canTriggerChange == true && m_volumeRate < isTriggerVolume)
-        {
-            // enemyScript.isTrigger = false;
-            // canTriggerChange = false;
-          //  falseTime = 10;
-            Debug.Log("VolF");
-        }
-    }
-}
+//#if UNITY_WEBGL && !UNITY_EDITOR
+//        void Update()
+//        {
+//            Microphone.Update();
+//        }
+//#endif
+
+//    void Start()
+//    {
+//        AudioSource aud = GetComponent<AudioSource>();
+//        if ((aud != null) && (Microphone.devices.Length > 0)) 
+//        {
+//            string devName = Microphone.devices[0]; 
+//            int minFreq, maxFreq;
+//            Microphone.GetDeviceCaps(devName, out minFreq, out maxFreq); 
+//            aud.clip = Microphone.Start(devName, true, 2, minFreq);
+//            aud.Play(); 
+//        }
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        //  Debug.Log(m_volumeRate);
+//        if (falseTime > 0)
+//        {
+//            falseTime -= Time.deltaTime;
+//        }
+//        else if(canTriggerChange == true && falseTime < 0)
+//        {
+//            enemyScript.isTrigger = false;
+//            canTriggerChange = false;
+//        }
+//    }
+
+//    // 
+//    private void OnAudioFilterRead(float[] data, int channels)
+//    {
+//        float sum = 0f;
+//        for (int i = 0; i < data.Length; ++i)
+//        {
+//            sum += Mathf.Abs(data[i]); //
+//        }
+//        //
+//        m_volumeRate = Mathf.Clamp01(sum * m_gain / (float)data.Length);
+//        if (m_volumeRate > isTriggerVolume )
+//        {
+//            enemyScript.isTrigger = true;
+//            canTriggerChange = true;
+//            falseTime = 10;
+//            Debug.Log("VolT");
+//        }
+//        else if(canTriggerChange == true && m_volumeRate < isTriggerVolume)
+//        {
+//            // enemyScript.isTrigger = false;
+//            // canTriggerChange = false;
+//          //  falseTime = 10;
+//            Debug.Log("VolF");
+//        }
+//    }
+//}
